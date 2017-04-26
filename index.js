1ef7d313428bf4b090713b08bac0a255c068b12b
jfinlays <jfinlays@jfinlayss-MacBook-Pro.local>
2017-04-27 01:39:05 +0200

    change errors to warnings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     "parser": "babel-eslint",
-    "extends": "airbnb",
+    "extends": "react-app",
     "env": {
         "browser": true,
         "node": true,
@@ -32,23 +32,23 @@ module.exports = {
     "rules": {
         "arrow-parens": [0, "as-needed"],
         "arrow-body-style": [0, "as-needed"],
-        "comma-dangle": [2, "never"],
+        "comma-dangle": [1, "never"],
         "global-require": 0,
         "import/imports-first": 0,
         "import/newline-after-import": 0,
         "import/no-extraneous-dependencies": 0,
         "import/no-named-as-default": 0,
-        "import/no-unresolved": [2, {"caseSensitive": true}],
+        "import/no-unresolved": [1, {"caseSensitive": true}],
         "import/prefer-default-export": 0,
-        "indent": [2, 2, { "SwitchCase": 1 }],
+        "indent": [1, 2, { "SwitchCase": 1 }],
         "key-spacing": ["error", { "afterColon": true, "mode": "minimum" }],
-        "jsx-a11y/aria-props": 2,
+        "jsx-a11y/aria-props": 1,
         "jsx-a11y/heading-has-content": 0,
-        "jsx-a11y/href-no-hash": 2,
-        "jsx-a11y/label-has-for": 2,
-        "jsx-a11y/mouse-events-have-key-events": 2,
-        "jsx-a11y/role-has-required-aria-props": 2,
-        "jsx-a11y/role-supports-aria-props": 2,
+        "jsx-a11y/href-no-hash": 1,
+        "jsx-a11y/label-has-for": 1,
+        "jsx-a11y/mouse-events-have-key-events": 1,
+        "jsx-a11y/role-has-required-aria-props": 1,
+        "jsx-a11y/role-supports-aria-props": 1,
         "max-len": 0,
         "newline-per-chained-call": 0,
         "no-case-declarations": 0,
@@ -61,7 +61,7 @@ module.exports = {
         "no-use-before-define": 0,
         "one-var": 0,
         "one-var-declaration-per-line": 0,
-        "prefer-template": 2,
+        "prefer-template": 1,
         "class-methods-use-this": 0,
         "react/forbid-prop-types": 0,
         "react/jsx-first-prop-new-line": [
@@ -87,6 +87,6 @@ module.exports = {
             1,
             "never"
         ],
-        "no-underscore-dangle": [2, { "allow": ["_component"] }]
+        "no-underscore-dangle": [1, { "allow": ["_component"] }]
     }
 }
