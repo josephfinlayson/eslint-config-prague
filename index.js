7e2c43191e1a26dc6592c3a9266650d630dd417d
jfinlays <jfinlays@jfinlayss-MacBook-Pro.local>
2017-04-27 00:02:32 +0200

    specify case dependency of imports

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ module.exports = {
         "import/newline-after-import": 0,
         "import/no-extraneous-dependencies": 0,
         "import/no-named-as-default": 0,
-        "import/no-unresolved": 2,
+        "import/no-unresolved": [2, {"caseSensitive": true}],
         "import/prefer-default-export": 0,
         "indent": [2, 2, { "SwitchCase": 1 }],
         "key-spacing": ["error", { "afterColon": true, "mode": "minimum" }],
